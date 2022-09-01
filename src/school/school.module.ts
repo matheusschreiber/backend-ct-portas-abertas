386cde99f9e7cb64c4a1fd1dfc27d20d246d9bb4
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-01 01:59:08 -0300

    :recycle: Updating all modules

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { SchoolController } from './school.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { School } from './entities/school.entity';
+import { Event } from '../events/entities/event.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([School, Event])],
   controllers: [SchoolController],
   providers: [SchoolService]
 })
--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
+import { Event } from '../events/entities/event.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Student, Event])],
   controllers: [StudentController],
   providers: [StudentService]
 })
