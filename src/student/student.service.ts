52505b6d1223fda9f4cadbde044f9a4e206c0379
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-01 16:59:07 -0300

    :recycle: Adding update events function

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { Student } from './entities/student.entity';
+import { Event } from '../events/entities/event.entity';
+import { EventsService } from '../events/events.service';
 
 @Injectable()
 export class StudentService {
@@ -11,6 +13,9 @@ export class StudentService {
   constructor(
     @InjectRepository(Student)
     private studentRepo: Repository<Student>,
+    @InjectRepository(Event)
+    private eventRepo: Repository<Event>,
+    private eventService: EventsService
   ){}
 
   create(createStudentDto: CreateStudentDto) {
@@ -23,11 +28,24 @@ export class StudentService {
   }
 
   findOne(id: number) {
-    return this.studentRepo.findOneBy({id: id})
+    return this.studentRepo.findOne({where: {id: id}, relations: ["events"]})
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+  async update(id: number, updateStudentDto: UpdateStudentDto) {
+
+    //ID do evento(objeto)
+    const eventID = await updateStudentDto.events[0]
+    //instância do evento que foi passado pelo ID do body
+    const eventbyid = await this.eventRepo.findOneBy(eventID)
+    //instância do estudante que será atualizado 
+    let student = await this.findOne(id)
+    //Adicionando o evento novo nos eventos do estudante
+    student.events.push(eventbyid)
+    
+    //atualizando o filled do evento
+    await this.eventService.updateFilled(eventID)
+
+    return await this.studentRepo.save(student)
   }
 
   remove(id: number) {
