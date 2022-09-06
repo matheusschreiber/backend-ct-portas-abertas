205f36f1a6aef3b6c5eb9abe9e9c1f860770c485
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-05 22:41:10 -0300

    :recycle: event service injected

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -4,10 +4,12 @@ import { SchoolController } from './school.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { School } from './entities/school.entity';
 import { Event } from '../events/entities/event.entity';
+import { EventsService } from '../events/events.service';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([School, Event])],
   controllers: [SchoolController],
-  providers: [SchoolService]
+  providers: [SchoolService, EventsService]
 })
 export class SchoolModule {}
