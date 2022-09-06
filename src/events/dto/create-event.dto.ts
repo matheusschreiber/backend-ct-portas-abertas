21d42bf46487ab52d50b0189fd2f4cec648ed40e
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-05 22:36:51 -0300

    :sparkles: Creating events DTO

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,14 @@
-export class CreateEventDto {}
+import { EventType } from "../entities/event.entity"
+
+export class CreateEventDto {
+
+    title: string
+
+    local:string
+
+    time: Date
+
+    capacity: number
+
+    type: EventType 
+}
