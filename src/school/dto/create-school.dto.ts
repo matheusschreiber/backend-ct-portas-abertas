38f9e8fafab51b4d8a7240af671f45435ca35226
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-05 22:37:24 -0300

    :sparkles: Creating school DTO and adding validations

--- src/school/dto/create-school.dto.ts
+++ src/school/dto/create-school.dto.ts
@@ -1,1 +1,26 @@
-export class CreateSchoolDto {}
+import { IsEmail, IsNotEmpty, IsNumber, Length } from "class-validator"
+
+export class CreateSchoolDto {
+
+    @IsNotEmpty()
+    name: string
+
+    @IsNotEmpty()
+    nameRes: string
+
+    @IsNotEmpty()
+    @Length(11,11,{message:"cpf inválido"})
+    cpfRes: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    studentsAmount: number
+
+    @IsNotEmpty()
+    @Length(8,255,{message:"senha inválida"})
+    password: string
+
+    @IsNotEmpty()
+    @IsEmail({},{message:"email inválido"})
+    emailRes: string
+}
